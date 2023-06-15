520e3084d821b8276ee2bd232c58275b4df3e9b4
skillbot101 <shresh.khatri@gmail.com>
2023-06-15 14:40:59 +0100

    botwidget.js settings updated

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const SERVER_URL='http://localhost:4000/botmanagement'
 export const AppTitle = ' - Chatbot Management web Portal'
 export const regexNLP= /^[a-zA-Z0-9_]*$/i;
+export const BOT_SERVER_URL = 'http://localhost'
--- src/pages/ChatbotWidget/BotWidget.js
+++ src/pages/ChatbotWidget/BotWidget.js
@@ -1,87 +1,116 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Widget, addResponseMessage, renderCustomComponent, addUserMessage, toggleInputDisabled, addLinkSnippet, dropMessages } from 'react-chat-widget';
-import { CustomButtonGroup, CustomTextField, EmailComponent } from './WidgetCustomComponents'
-import { decode } from 'html-entities'
+import { useNavigate } from 'react-router-dom';
+import { CustomButtonGroup, CustomTextField,EmailComponent } from './WidgetCustomComponents'
+import {decode} from 'html-entities'
+import Box from '@mui/material/Box';
 import CustomSnackbar from '../../components/CustomSnackbar';
 import { useAppStateDispatch, useAppStateContext } from '../../ApplicationContextProvider'
 import 'react-chat-widget/lib/styles.css';
+import { BOT_SERVER_URL } from '../../config';
 
-export default function BotWidget({ props, children }) {
+export default function BotWidget({props,children}) {
   const dispatch = useAppStateDispatch()
   const appState = useAppStateContext()
-  const [conversationID, setConversationID] = useState('')
-  const [botName, setBotName] = useState('')
+  const [conversationID, setConversationID] = useState()
+  const [botName,setBotName] = useState('')
   const [snackBarPayload, setSnackBarPayload] = useState({ open: false, severity: '', message: '' })
-  const [currentBotServerPort, setCurrentBotServerPort] = useState(null)
-  const [URL_GET_CONVERSATION_ID, setURL_GET_CONVERSATION_ID] = useState()
-  const [COMMUNICATION_ENDPOINT, setCOMMUNICATION_ENDPOINT] = useState(`http://localhost:${currentBotServerPort}/directline/conversations/${conversationID}/activities`)
+  const [currentBotServerPort,setCurrentBotServerPort] = useState()
+  const [URL_GET_CONVERSATION_ID,setURL_GET_CONVERSATION_ID] = useState(null)
+  const [COMMUNICATION_ENDPOINT,setCOMMUNICATION_ENDPOINT] = useState()
   console.log(conversationID)
+  console.log(URL_GET_CONVERSATION_ID)
   console.log(COMMUNICATION_ENDPOINT)
+  console.log('Widget reloaded')
+  
 
-  useEffect(()=>{
-    if (appState.hasOwnProperty('settings')){
-      setBotName(decode(appState.settings.botName))
-      setCurrentBotServerPort(appState.settings.currentBotServerPort)
-    }
-
-  },[appState]);
-
-  useEffect(() => {
-    if (!currentBotServerPort){
-      return;
-    }
-    setURL_GET_CONVERSATION_ID(`http://localhost:${currentBotServerPort}/directline/conversations`)
-    
-  }, [currentBotServerPort])
-
+  
   useEffect(async () => {
-    if (!URL_GET_CONVERSATION_ID){
+    if (!appState.hasOwnProperty('settings')){
       return;
     }
-    await getConversationID()
+    setBotName(decode(appState.settings.botName))
+    setCurrentBotServerPort(appState.settings.currentBotServerPort)
+    setURL_GET_CONVERSATION_ID(`${BOT_SERVER_URL}:${currentBotServerPort}/directline/conversations`)
+
+  }, [appState])
+
+  
+  useEffect(async () => {
+    if (!URL_GET_CONVERSATION_ID || URL_GET_CONVERSATION_ID.includes('undefined')){
+      return;
+    }
+      await getConversationID()
+    
   }, [URL_GET_CONVERSATION_ID])
 
 
 
-  async function getConversationID() {
-    await fetch(URL_GET_CONVERSATION_ID, {
-      method: "POST",
-      redirect: 'follow',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'charset': 'UTF-8'
-      },
-      credentials: 'include'
-    }).then(async response => {
-      var json = await response.json()
-      json.status = response.status
-      return json
-    }).then((jsondata) => {
-      if (jsondata.status === 200) {
-        setCOMMUNICATION_ENDPOINT(`http://localhost:${currentBotServerPort}/directline/conversations/${jsondata.conversationId}/activities`)
-      }
-      else {
-        setSnackBarPayload({
-          open: true,
-          severity: "error",
-          message: 'Server error occured while generating the conversation ID.'
-        })
-      }
-    })
-      .catch(error => {
-        setSnackBarPayload({
-          open: true,
-          severity: "error",
-          message: 'Connection failed while obtaining conversation ID for the ChatBot widget.'
-        })
-      });
 
+  // an effect to display some information in case obtaining the conversation ID process fails
+  useEffect(() => {
+    if (!conversationID) {
+      return;
+    }
+    else{
+      setCOMMUNICATION_ENDPOINT(`${BOT_SERVER_URL}:${currentBotServerPort}/directline/conversations/${conversationID}/activities`)
+    }
+  }, [conversationID]);
+
+
+  async function getConversationID(){
+      await fetch(URL_GET_CONVERSATION_ID, {
+        method: "POST",
+        redirect: 'follow',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'charset': 'UTF-8'
+        },
+        credentials:'include'
+      }).then(async response => {
+        var json = await response.json()
+        json.status = response.status
+        return json
+      }).then((jsondata) => {
+        if (jsondata.status === 200) {
+          setConversationID(jsondata.conversationId && jsondata.conversationId);
+        }
+        else {
+          setConversationID(null)
+          setSnackBarPayload({
+            open: true,
+            severity: "error",
+            message: 'Server error occured while generating the conversation ID.'
+          })
+        }
+      })
+        .catch(error => {
+          setSnackBarPayload({
+            open: true,
+            severity: "error",
+            message: 'Connection failed while obtaining conversation ID for the ChatBot widget.'
+          })
+        });
+    
   }
 
   function closeSnackbar() {
     setSnackBarPayload({ open: false, severity: 'info', message: '' })
   }
+
+
+  
+  // an effect to display some information in case obtaining the conversation ID process fails
+  useEffect(() => {
+    if (!conversationID) {
+      addResponseMessage('Hi there\n\nSorry but obtaining conversation ID just failed\n\nYou might need to add some training data and train the model and referesh the page again.\n\nOr Please ensure that the bot is up and running.')
+      
+    }else{
+      dropMessages()
+      
+    }
+  }, [conversationID]);
 
   //function to send mesage to the server
   async function sendMessage(message) {
@@ -134,6 +163,7 @@ export default function BotWidget({ props, children }) {
   }
 
 
+
   //procecss each message activity
   function processActivity(activity) {
 
@@ -142,11 +172,14 @@ export default function BotWidget({ props, children }) {
       if (activity.hasOwnProperty('text')) {
         addResponseMessage(activity.text)
       }
+
       //ensuring that the activity has payload property
       if (activity.hasOwnProperty('payload')) {
         processPayload(activity.payload)
       }
     }
+
+
   }
 
   //function to process payload present in payload field in activity
